73b5b79eda8d4ede76aff488814d36ed65d86e53
Aqudi <gjdigj145@gmail.com>
2020-11-13 00:33:42 +0900

    feat: Add id property

--- algo_sports/users/models.py
+++ algo_sports/users/models.py
@@ -1,6 +1,5 @@
 from django.contrib.auth.models import AbstractUser
 from django.db.models import AutoField, CharField
-from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
 
 
@@ -11,11 +10,6 @@ class User(AbstractUser):
     name = CharField(_("Name of User"), blank=True, max_length=255)
     language = CharField(_("Language of User"), blank=True, max_length=30)
 
-    def get_absolute_url(self):
-        """Get url for user's detail view.
-
-        Returns:
-            str: URL for user detail.
-
-        """
-        return reverse("users:detail", kwargs={"username": self.username})
+    @property
+    def id(self):
+        return self.user_id
