cb68340f4ffb1e4ac6d9e8ba19622affbf665a15
Aqudi <gjdigj145@gmail.com>
2020-11-17 20:40:40 +0900

    feat: Add api routers

--- config/api_router.py
+++ config/api_router.py
@@ -1,6 +1,8 @@
 from django.conf import settings
 from rest_framework.routers import DefaultRouter, SimpleRouter
 
+from algo_sports.codes.views import JudgementCodeViewSet, UserCodeViewSet
+from algo_sports.games.views import GameInfoViewSet, GameRoomViewSet
 from algo_sports.users.views import UserViewSet
 
 if settings.DEBUG:
@@ -9,6 +11,10 @@ else:
     router = SimpleRouter()
 
 router.register("users", UserViewSet)
+router.register("games/info", GameInfoViewSet)
+router.register("games/room", GameRoomViewSet)
+router.register("codes/user", UserCodeViewSet)
+router.register("codes/judegement", JudgementCodeViewSet)
 
 
 app_name = "api"
