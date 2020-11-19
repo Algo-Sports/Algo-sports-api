035cb299d4e12971cc81f4a6055fd86d79c03ca7
Aqudi <gjdigj145@gmail.com>
2020-11-20 02:59:23 +0900

    feat: Register blogs app urls to api_router

--- config/api_router.py
+++ config/api_router.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from rest_framework.routers import DefaultRouter, SimpleRouter
 
+from algo_sports.blogs.views import BlogViewSet, CommentViewSet, PostViewSet
 from algo_sports.users.views import UserViewSet
 
 if settings.DEBUG:
@@ -9,6 +10,9 @@ else:
     router = SimpleRouter()
 
 router.register("users", UserViewSet)
+router.register("blogs", BlogViewSet)
+router.register("posts", PostViewSet)
+router.register("comments", CommentViewSet)
 
 
 app_name = "api"
