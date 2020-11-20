a9412d4f4d9a6b24a3c24decabe57fbfaa92a9c1
Aqudi <gjdigj145@gmail.com>
2020-11-20 18:37:40 +0900

    feat: Create codes serializers

---
+++ algo_sports/codes/serializers.py
@@ -0,0 +1,47 @@
+from rest_framework import serializers
+
+from algo_sports.users.serializers import UsernameSerializer
+
+from .models import CodeRoomRelation, JudgementCode, UserCode
+
+
+class UserCodeSerializer(serializers.ModelSerializer):
+    user = UsernameSerializer(read_only=True)
+
+    class Meta:
+        model = UserCode
+        fields = [
+            "user",
+            "programming_language",
+            "code",
+            "created_at",
+            "updated_at",
+        ]
+
+
+class JudgementCodeSerializer(serializers.ModelSerializer):
+    user = UsernameSerializer(read_only=True)
+
+    class Meta:
+        model = JudgementCode
+        fields = [
+            "user",
+            "gameinfo_id",
+            "programming_language",
+            "code",
+            "created_at",
+            "updated_at",
+        ]
+
+
+class CodeRoomRelationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CodeRoomRelation
+        fields = [
+            "usercode",
+            "gameroom",
+            "score",
+            "history",
+            "created_at",
+            "updated_at",
+        ]
