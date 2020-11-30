6057bbd98700b5185580fdb50dd31316cc4db6c1
Aqudi <gjdigj145@gmail.com>
2020-11-30 13:15:52 +0900

    feat: Update version manage functions

--- algo_sports/games/models.py
+++ algo_sports/games/models.py
@@ -1,4 +1,5 @@
 from enum import Enum
+from typing import Union
 
 from django.db import models
 from django.utils.translation import gettext_lazy as _
@@ -12,11 +13,11 @@ class GameVersionType(str, Enum):
     micro = "micro"
 
 
-def get_default_version():
+def make_version(major=0, minor=0, micro=1):
     return {
-        GameVersionType.major: 0,
-        GameVersionType.minor: 0,
-        GameVersionType.micro: 1,
+        GameVersionType.major: major,
+        GameVersionType.minor: minor,
+        GameVersionType.micro: micro,
     }
 
 
@@ -40,29 +41,57 @@ class GameInfo(models.Model):
     def __str__(self) -> str:
         return f"{self.title} ({self.total_versions})"
 
+    def get_versions(self, ordering=False):
+        versions = self.versions.all()
+        if ordering:
+            versions = versions.order_by(
+                "-version__major",
+                "-version__minor",
+                "-version__micro",
+            )
+        return versions
+
     @property
     def is_active(self):
         return self.total_versions > 0
 
     @property
     def change_log(self):
-        return self.versions.all().values_list("change_log")
+        return self.get_versions(ordering=True).values_list("version", "change_log")
 
     @property
     def latest_version(self):
-        return (
-            self.versions.all()
-            .order_by(
-                "-version__major",
-                "-version__minor",
-                "-version__micro",
-            )
-            .first()
-        )
+        return self.get_versions(ordering=True).first()
 
     @property
     def total_versions(self):
         return self.versions.all().count()
+
+    def update_version(
+        self,
+        update_type: GameVersionType,
+        change_log: list,
+        target_version: dict = None,
+    ):
+        if not self.is_active:
+            # 게임 정보만 존재할 시 새로 버전을 생성
+            GameVersion.objects.create(gameinfo_id=self, version=make_version(0, 0, 0))
+
+        version = None
+        if target_version:
+            version.get_versions(ordering=True).filter(version=target_version).first()
+        else:
+            version = self.latest_version
+
+        if update_type == GameVersionType.major:
+            version = version.major_up()
+        elif update_type == GameVersionType.minor:
+            version = version.minor_up()
+        elif update_type == GameVersionType.micro:
+            version = version.micro_up()
+        else:
+            raise "Unexpected update type"
+        return version.update_change_log(change_log)
 
 
 class GameVersion(models.Model):
@@ -73,7 +102,7 @@ class GameVersion(models.Model):
         related_name="versions",
     )
 
-    version = models.JSONField(_("Game Version"), default=get_default_version)
+    version = models.JSONField(_("Game Version"), default=make_version)
     change_log = models.JSONField(_("Version change log"), default=dict)
     is_active = models.BooleanField(_("Is this version active?"), default=True)
 
@@ -103,19 +132,33 @@ class GameVersion(models.Model):
         return version
 
     def major_up(self):
+        self.pk = None
         self.version[GameVersionType.major] += 1
         self.version[GameVersionType.minor] = 0
         self.version[GameVersionType.micro] = 0
+        self.change_log = {}
         self.save()
+        return self
 
     def minor_up(self):
         self.version[GameVersionType.minor] += 1
         self.version[GameVersionType.micro] = 0
         self.save()
+        return self
 
     def micro_up(self):
         self.version[GameVersionType.micro] += 1
         self.save()
+        return self
+
+    def update_change_log(self, changes: Union[str, list]):
+        self.change_log.setdefault(self.version_str, [])
+        if isinstance(changes, str):
+            self.change_log.get(self.version_str).append(changes)
+        else:
+            self.change_log.get(self.version_str).extend(changes)
+        self.save()
+        return self
 
 
 class GameRoom(models.Model):
