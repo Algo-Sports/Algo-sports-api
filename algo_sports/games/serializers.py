2311c99b7764cb087813d2ac321f8a8fa387eefb
Aqudi <gjdigj145@gmail.com>
2020-12-03 01:46:03 +0900

    feat: Update gameinfo view and serializers

--- algo_sports/games/serializers.py
+++ algo_sports/games/serializers.py
@@ -1,21 +1,55 @@
 from rest_framework import serializers
 
-from .models import GameInfo, GameRoom
+from .models import GameInfo, GameRoom, GameVersion, GameVersionType
+
+
+class GameVersionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = GameVersion
+        exclude = ["change_log"]
 
 
 class GameInfoSerializer(serializers.ModelSerializer):
+    latest_version = serializers.SerializerMethodField(method_name="_version")
+
     class Meta:
         model = GameInfo
-        fields = [
-            "title",
-            "version",
-            "description",
-            "min_users",
-            "max_users",
-            "extra_info",
-            "created_at",
-            "updated_at",
-        ]
+        fields = "__all__"
+
+    def _version(self, instance):
+        version = instance.latest_version.version_str
+        return version
+
+
+class GameInfoDetailSerializer(serializers.ModelSerializer):
+    change_log = serializers.JSONField()
+    version = serializers.SerializerMethodField(method_name="_version")
+
+    class Meta:
+        model = GameInfo
+        fields = "__all__"
+
+    def _version(self, instance):
+        version = instance.latest_version
+        print(version)
+        return GameVersionSerializer(instance=version).data
+
+
+class GameInfoUpdateSerializer(serializers.ModelSerializer):
+    update_type = serializers.ChoiceField(
+        choices=GameVersionType.choices, required=False
+    )
+    change_log = serializers.ListField(required=False)
+
+    class Meta:
+        model = GameInfo
+        fields = "__all__"
+        read_only_fields = ["title", "description"]
+
+    def validate(self, attrs):
+        if attrs["max_users"] > attrs["min_users"]:
+            raise serializers.ValidationError("max_users가 min_users 보다 작습니다.")
+        return attrs
 
 
 class GameRoomSerializer(serializers.ModelSerializer):
--- algo_sports/games/views.py
+++ algo_sports/games/views.py
@@ -1,33 +1,82 @@
 from django.contrib.auth import get_user_model
 from rest_framework import status
 from rest_framework.decorators import action
-from rest_framework.mixins import ListModelMixin, RetrieveModelMixin
+from rest_framework.mixins import (
+    CreateModelMixin,
+    ListModelMixin,
+    RetrieveModelMixin,
+    UpdateModelMixin,
+)
 from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet
 
 from algo_sports.utils.permissions import IsAdminOrReadOnly
 
-from .models import GameInfo, GameRoom
-from .serializers import GameInfoSerializer, GameRoomSerializer
+from .models import GameInfo, GameRoom, GameVersionType
+from .serializers import (
+    GameInfoDetailSerializer,
+    GameInfoSerializer,
+    GameInfoUpdateSerializer,
+    GameRoomSerializer,
+)
 
 # from django.utils.translation import gettext_lazy as _
 
 User = get_user_model()
 
 
-class GameInfoViewSet(ListModelMixin, RetrieveModelMixin, GenericViewSet):
+class GameInfoViewSet(
+    ListModelMixin,
+    CreateModelMixin,
+    UpdateModelMixin,
+    RetrieveModelMixin,
+    GenericViewSet,
+):
     serializer_class = GameInfoSerializer
     queryset = GameInfo.objects.all()
     lookup_field = "pk"
     permission_classes = [IsAdminOrReadOnly]
 
-    @action(detail=False, methods=["POST"])
-    def register_game(self, request):
-        return Response(status=status.HTTP_201_CREATED)
+    action_serializer_classes = {
+        "retrieve": GameInfoDetailSerializer,
+        "update": GameInfoUpdateSerializer,
+        "partial_update": GameInfoUpdateSerializer,
+    }
 
-    @action(detail=True, methods=["POST"])
-    def update_game(self, request):
-        return Response(status=status.HTTP_201_CREATED)
+    def get_serializer_class(self):
+        serializer = self.action_serializer_classes.get(self.action)
+        if serializer:
+            return serializer
+        return super().get_serializer_class()
+
+    def update(self, request, *args, **kwargs):
+        """
+        GameInfo의 Version을 한 단계 올린다.
+        """
+        gameinfo = self.get_object()
+        update_type = request.data.get("update_type", GameVersionType.micro)
+        change_log = request.data.get("change_log")
+
+        request.data.pop("update_type")
+        request.data.pop("change_log")
+
+        if change_log:
+            gameinfo.update_version(
+                update_type=update_type,
+                change_log=change_log,
+            )
+            return super().update(request, *args, **kwargs)
+
+        return Response(
+            data={"detail": "Change log is required"},
+            status=status.HTTP_400_BAD_REQUEST,
+        )
+
+    @action(detail=True, methods=["GET"])
+    def get_active_versions(self, request):
+        queryset = self.queryset.get_versions(ordering=True)
+        queryset = queryset.filter(is_active=True)
+        return Response(status=status.HTTP_200_OK)
 
 
 class GameRoomViewSet(ListModelMixin, RetrieveModelMixin, GenericViewSet):
