4a40f185d8e5e9fef478dadbda56a1784f6237ed
Aqudi <gjdigj145@gmail.com>
2020-12-07 14:25:51 +0900

    feat: Reset codes admins

--- algo_sports/codes/admin.py
+++ algo_sports/codes/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import MatchCodeRelation, JudgementCode, ProgrammingLanguage, UserCode
+from .models import JudgementCode, MatchCodeRelation, ProgrammingLanguage, UserCode
 
 
 @admin.register(ProgrammingLanguage)
@@ -11,57 +11,60 @@ class ProgrammingLanguageAdmin(admin.ModelAdmin):
 
 @admin.register(UserCode)
 class UserCodeAdmin(admin.ModelAdmin):
-    list_display = (
-        "id",
-        "user_id",
-        "programming_language",
-        "code",
-        "is_active",
-        "created_at",
-        "updated_at",
-    )
-    list_filter = ("user_id", "programming_language", "created_at", "updated_at")
-    search_fields = ["author"]
-    raw_id_fields = ("gamematchs",)
-    date_hierarchy = "created_at"
+    # list_display = (
+    #     "id",
+    #     "user_id",
+    #     "programming_language",
+    #     "code",
+    #     "is_active",
+    #     "created_at",
+    #     "updated_at",
+    # )
+    # list_filter = ("user_id", "programming_language", "created_at", "updated_at")
+    # search_fields = ["author"]
+    # raw_id_fields = ("gamematchs",)
+    # date_hierarchy = "created_at"
 
-    def author(self, obj):
-        return obj.user.username
+    # def author(self, obj):
+    #     return obj.user.username
+    pass
 
 
 @admin.register(JudgementCode)
 class JudgementCodeAdmin(admin.ModelAdmin):
-    list_display = (
-        "id",
-        "user_id",
-        "gameinfo_id",
-        "programming_language",
-        "code",
-        "created_at",
-        "updated_at",
-    )
-    list_filter = (
-        "user_id",
-        "gameinfo_id",
-        "programming_language",
-        "created_at",
-        "updated_at",
-    )
-    search_fields = ("author", "gameinfo__title")
-    date_hierarchy = "created_at"
+    # list_display = (
+    #     "id",
+    #     "user_id",
+    #     "gameinfo_id",
+    #     "programming_language",
+    #     "code",
+    #     "created_at",
+    #     "updated_at",
+    # )
+    # list_filter = (
+    #     "user_id",
+    #     "gameinfo_id",
+    #     "programming_language",
+    #     "created_at",
+    #     "updated_at",
+    # )
+    # search_fields = ("author", "gameinfo__title")
+    # date_hierarchy = "created_at"
 
-    def author(self, obj):
-        return obj.user.username
+    # def author(self, obj):
+    #     return obj.user.username
+    pass
 
 
 @admin.register(MatchCodeRelation)
 class MatchCodeRelationAdmin(admin.ModelAdmin):
-    list_display = (
-        "id",
-        "usercode_id",
-        "gamematch_id",
-        "created_at",
-        "updated_at",
-    )
-    list_filter = ("usercode_id", "gamematch_id", "created_at", "updated_at")
-    date_hierarchy = "created_at"
+    # list_display = (
+    #     "id",
+    #     "usercode_id",
+    #     "gamematch_id",
+    #     "created_at",
+    #     "updated_at",
+    # )
+    # list_filter = ("usercode_id", "gamematch_id", "created_at", "updated_at")
+    # date_hierarchy = "created_at"
+    pass
