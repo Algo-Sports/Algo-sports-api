64227e64fb3d7f0c5f825a5f87da8366cab2c189
Aqudi <gjdigj145@gmail.com>
2020-12-13 04:20:46 +0900

    feat: Update run_match method
    코드 저장 구조 변경
    언어별 main 코드 렌더링을 하도록 수정

--- algo_sports/games/tasks.py
+++ algo_sports/games/tasks.py
@@ -1,4 +1,5 @@
 import subprocess
+from pathlib import Path
 
 from django.contrib.auth import get_user_model
 from django.shortcuts import get_object_or_404
@@ -17,6 +18,9 @@ def run_match(match_data):
     competitors = UserCode.objects.filter(id__in=match_data.get("competitor_ids"))
     # print(room, match, competitors)
 
+    default_setting = room.gameversion.default_setting
+    print(default_setting)
+
     for competitor in competitors:
         language = competitor.programming_language
 
@@ -24,14 +28,23 @@ def run_match(match_data):
         run_cmd = language.run_cmd
         compile_cmd = language.compile_cmd
 
+        # main 렌더링
+        includes = default_setting["includes"][language.name]
+        arguments = default_setting["arguments"][language.name]
+        solution = competitor.code
+        code_string = str(language.get_main_template(includes, arguments, solution))
+
         # 코드 파일 생성
-        filename = f"room_{room.id}_match_{match.id}_code_{competitor.id}"
-        file = open(filename, "w")
-        gen_code = ["echo", f"{competitor.code}"]
-        subprocess.run(gen_code, stdout=file, check=True)
+        parent = Path(f"match_datas/room_{room.id}/match_{match.id}/")
+        parent.mkdir(parents=True, exist_ok=True)
+        file_name = f"code_{competitor.id}"
+        file_path = parent / file_name
+        with file_path.open("w", encoding="utf-8") as f:
+            gen_code = ["echo", f"{code_string}"]
+            subprocess.run(gen_code, stdout=f, check=True)
 
         # 커맨드 생성
-        run_code = ["./run.sh", "-r", f"{run_cmd}", "-s", f"{filename}"]
+        run_code = ["./run.sh", "-r", f"{run_cmd}", "-s", f"{file_path}"]
         if compile_cmd:
             run_code.extend(["-c", f"{compile_cmd}"])
 
