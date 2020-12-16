20356174f0c2f758f5876713201c68d86aa2df63
Aqudi <gjdigj145@gmail.com>
2020-12-17 01:07:51 +0900

    fix: Deploy settings
    ALLOWED_HOSTS
    print(DEBUG)
    Nginx configs
    compose setting

--- config/urls.py
+++ config/urls.py
@@ -5,20 +5,11 @@ from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import include, path, re_path
 from django.views import defaults as default_views
-from django.views.generic import TemplateView
 
 urlpatterns = [
-    path("", TemplateView.as_view(template_name="pages/home.html"), name="home"),
-    path(
-        "about/", TemplateView.as_view(template_name="pages/about.html"), name="about"
-    ),
     # Django Admin, use {% raw %}{% url 'admin:index' %}{% endraw %}
     path(settings.ADMIN_URL, admin.site.urls),
     # User management
-    path(
-        "users/",
-        include("algo_sports.users.urls", namespace="users"),
-    ),
     path("accounts/", include("allauth.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 if settings.DEBUG:
